04d4f11b27a41b9a0819fd017dc1a625efb12edf
GaryGolf <garygolf@mail.ru>
2018-09-12 20:48:53 +0300

    minor changes

--- src/components/hello.ts
+++ src/components/hello.ts
@@ -1,8 +1,6 @@
 import Vue from 'vue';
 import Component from 'vue-class-component';
-@Component({
-  template: '<button @click="onClick">Click!</button>'
-})
+@Component
 export default class Hello extends Vue {
   message: string = 'hello!';
   onClick = ():void =>  window.alert(this.message);
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,28 @@
 import Vue from 'vue';
-import Hello from './components/hello';
+import Hello from './components/hello.vue';
+import Component from 'vue-class-component';
 
-new Vue({
- render: createElement => createElement(Hello)
-}).$mount('#app');
+// new Vue({
+//  render: createElement => createElement(Hello)
+// }).$mount('#app');
+
+// new Vue({
+//   el: '#app',
+//   components: {
+//     Hello
+//   }
+// })
+
+// @Component({
+//   components: {
+//     Hello
+//   }
+// })
+@Component
+class App extends Vue {
+  mounted () {
+    console.log('mounted')
+  }
+}
+
+export const app = new App().$mount('#app');
