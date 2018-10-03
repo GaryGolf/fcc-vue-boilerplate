9d2306daf1fbd31cd7cc92b41f0e29869343388d
GaryGolf <garygolf@mail.ru>
2018-10-03 09:25:12 +0300

    add message to hello component

--- src/components/hello.ts
+++ src/components/hello.ts
@@ -1,6 +1,6 @@
 import { Vue, Component} from 'vue-property-decorator';
 
 @Component export default class Hello extends Vue {
-  message: string = 'hello!';
+  message: string = 'Hello Vue!';
   onClick = ():void =>  window.alert(this.message);
 }
