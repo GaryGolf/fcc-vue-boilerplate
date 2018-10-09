bcd1f716a0e79c46210bccfca50ea06abbc20ea4
GaryGolf <garygolf@mail.ru>
2018-10-09 12:18:10 +0300

    update description component

--- src/components/description.vue
+++ src/components/description.vue
@@ -1,3 +1,20 @@
-<template src="./description.html" />
-<script lang="ts" src"./description.ts"/>
+<template>
+  <pre>{{ message }}</pre>
+</template>
+  
+<script lang="ts" >
+  export default {
+    name: 'qq-description',
+    data() {
+      return {
+        message:  'Привет'
+      }
+    }
+  }
+</script>
+<style>
+pre {
+  color: greenyellow;
+}
+</style>
 
