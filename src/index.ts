3b1aaf0d34158f9566cf7803bc64970ecd9164f5
GaryGolf <garygolf@mail.ru>
2018-10-24 20:50:12 +0300

    add store

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import Vue from 'vue';
 import Hello from './components/hello.vue';
+import Vuex from 'vuex';
 
 Vue.config.productionTip = false;
 
+const store = new Vuex.Store({
+  state: {
+    message: 'hello'
+  },
+  mutations: {
+    change (state) {
+      state.message = 'hello vuex'
+    }
+  }
+})
+
 new Vue({
+  store,
   render: create => create(Hello)
 }).$mount('#app')
